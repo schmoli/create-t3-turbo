e3b7015896c1a9f064ff6b096bfc0020cb7988bf
Toli Leonovich <schmoli@gmail.com>
2023-02-12 15:22:10 -0800

    adding ui to nextjs

--- apps/nextjs/src/pages/_app.tsx
+++ apps/nextjs/src/pages/_app.tsx
@@ -1,7 +1,8 @@
-import "../styles/globals.css";
-import type { AppType } from "next/app";
 import type { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+import type { AppType } from "next/app";
+import "../styles/globals.css";
+// import "@acme/ui/styles.css"
 
 import { api } from "~/utils/api";
 
