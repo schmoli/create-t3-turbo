5356a31f57fdab390d55e9c946bb8acf3d3a481b
Toli Leonovich <schmoli@gmail.com>
2023-02-12 15:36:06 -0800

    supporting styles.css in ui package!

--- apps/nextjs/src/pages/_app.tsx
+++ apps/nextjs/src/pages/_app.tsx
@@ -2,7 +2,7 @@ import type { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import type { AppType } from "next/app";
 import "../styles/globals.css";
-// import "@acme/ui/styles.css"
+import "@acme/ui/styles.css";
 
 import { api } from "~/utils/api";
 
--- apps/nextjs/src/pages/index.tsx
+++ apps/nextjs/src/pages/index.tsx
@@ -2,7 +2,7 @@ import { Button, ClassComponent, FunctionalComponent } from "@acme/ui";
 
 export default function ReactComponent() {
   return (
-    <div className="flex h-screen flex-col items-center justify-center bg-gray-300 text-green-700">
+    <div className="flex h-screen flex-col items-center justify-center">
       <h2 className="text-orange-700">it works!</h2>
       <Button>This is a button</Button>
       <FunctionalComponent>This is a functional Component</FunctionalComponent>
